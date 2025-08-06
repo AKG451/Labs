5d21e53b5ea737c3633ff5a9bf604195c87f07d9
AKG451 <akg451154@gmail.com>
2025-08-06 12:50:57 +0530

    lab2 experiment 3 added

--- L2E1.c
+++ L2E1.c
@@ -1,13 +1,15 @@
 # include <stdio.h>
 
 int main(){
-int n;
-printf("Enter the number: ");
-scanf("%d",&n);
-if(n%2==0){
+    int n;
+
+    printf("Enter the number: ");
+    scanf("%d",&n);
+
+  if(n%2==0){
     printf("%d is an even number\n",n);
 }
-else{printf("%d is an odd number\n",n);
+  else{printf("%d is an odd number\n",n);
 }
-return 0;
+  return 0;
 }
