e89f347dc5e61a1a8fdfa204032570a51c53fc5f
AKG451 <akg451154@gmail.com>
2025-08-25 10:03:45 +0530

    Added range check

---
+++ quiz1q10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+char c = "A";
+printf("%c",c);
+return 0;
+
+}
+
---
+++ range.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+
+int var = sizeof(int);
+printf("%d",var);
+return 0;
+}
