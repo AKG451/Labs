a869553167d954189b35fa8d58ff439b04cc455d
AKG451 <akg451154@gmail.com>
2025-08-28 00:28:23 +0530

    changed a fundamental flaw

--- L5E4.c
+++ L5E4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int element_to_delete, position = -1;
+    int position_to_delete;
 
     int my_array[20] = {64, 34, 25, 12, 22, 11, 90};
     int array_size = 7;
@@ -12,31 +12,23 @@ int main() {
     }
     printf("\n\n");
 
-    printf("Enter the element you want to delete: ");
-    scanf("%d", &element_to_delete);
+    printf("Enter the position of the element you want to delete (0 to %d): ", array_size - 1);
+    scanf("%d", &position_to_delete);
 
-    for (int i = 0; i < array_size; i++) {
-        if (my_array[i] == element_to_delete) {
-            position = i;
-            break;
-        }
-    }
-
-    if (position != -1) {
-        for (int i = position; i < array_size - 1; i++) {
+    if (position_to_delete >= 0 && position_to_delete < array_size) {
+        for (int i = position_to_delete; i < array_size - 1; i++) {
             my_array[i] = my_array[i + 1];
         }
         array_size--;
 
-        printf("\nArray after deleting %d:\n", element_to_delete);
+        printf("\nArray after deleting the element at position %d:\n", position_to_delete);
         for (int i = 0; i < array_size; i++) {
             printf("%d ", my_array[i]);
         }
         printf("\n");
     } else {
-        printf("\nElement %d not found in the array.\n", element_to_delete);
+        printf("\nInvalid position! Please enter a position between 0 and %d.\n", array_size - 1);
     }
 
     return 0;
 }
-
