6f4bbb760272fbabd81e2db46954c27b3270d109
AKG451 <akg451154@gmail.com>
2025-08-27 18:32:07 +0530

    changed some variables

--- L5E3.c
+++ L5E3.c
@@ -4,35 +4,35 @@ int main() {
     char order_choice;
     int new_element, position;
 
-    int my_array[20] = {64, 34, 25, 12, 22, 11, 90};
-    int array_size = 7;
+    int array[20] = {64, 34, 25, 12, 22, 11, 90};
+    int size = 7;
 
     printf("Original array: ");
-    for (int i = 0; i < array_size; i++) {
-        printf("%d ", my_array[i]);
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
     }
     printf("\n\n");
 
     printf("Enter the element to insert: ");
     scanf("%d", &new_element);
 
-    printf("Enter the position (0 to %d) where you want to insert it: ", array_size);
+    printf("Enter the position (0 to %d) where you want to insert it: ", size);
     scanf("%d", &position);
 
-    if (position < 0 || position > array_size) {
+    if (position < 0 || position > size) {
         printf("Invalid position!\n");
         return 1;
     } else {
-        for (int i = array_size - 1; i >= position; i--) {
-            my_array[i + 1] = my_array[i];
+        for (int i = size - 1; i >= position; i--) {
+            array[i + 1] = array[i];
         }
 
-        my_array[position] = new_element;
-        array_size++;
+        array[position] = new_element;
+        size++;
 
         printf("\nArray after inserting %d at position %d:\n", new_element, position);
-        for (int i = 0; i < array_size; i++) {
-            printf("%d ", my_array[i]);
+        for (int i = 0; i < size; i++) {
+            printf("%d ", array[i]);
         }
         printf("\n");
     }
@@ -40,21 +40,21 @@ int main() {
     printf("\nEnter 'a' for ascending order or 'd' for descending order: ");
     scanf(" %c", &order_choice);
 
-    for (int i = 0; i < array_size - 1; i++) {
-        for (int j = 0; j < array_size - i - 1; j++) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
             switch (order_choice) {
                 case 'a':
-                    if (my_array[j] > my_array[j + 1]) {
-                        int temp = my_array[j];
-                        my_array[j] = my_array[j + 1];
-                        my_array[j + 1] = temp;
+                    if (array[j] > array[j + 1]) {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
                     }
                     break;
                 case 'd':
-                    if (my_array[j] < my_array[j + 1]) {
-                        int temp = my_array[j];
-                        my_array[j] = my_array[j + 1];
-                        my_array[j + 1] = temp;
+                    if (array[j] < array[j + 1]) {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
                     }
                     break;
             }
@@ -63,8 +63,8 @@ int main() {
 
     if (order_choice == 'a' || order_choice == 'd') {
         printf("\nThe final sorted array is:\n");
-        for (int i = 0; i < array_size; i++) {
-            printf("%d ", my_array[i]);
+        for (int i = 0; i < size; i++) {
+            printf("%d ", array[i]);
         }
         printf("\n");
     } else {
